185539c32713044092791fc9ad8afa774b7516da
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-27 12:47:22 -0300

    Initial Commit

---
+++ tarjeta de presentacion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarjeta de presentacion
+//
+//  Created by Selena Belén Garcia Lobo on 27/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
